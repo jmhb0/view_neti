8dd8e27efcc06d762deb9a56e8ab81e01a761eaf
jmhb0 <jamesmhburgess1@gmail.com>
2023-11-14 17:36:05 -0800

    Update pretrained_models.py

--- training/pretrained_models.py
+++ training/pretrained_models.py
@@ -1,5 +1,5 @@
 lookup_pretrained_models = {
 	'0' : None,
 	'1' : "results/mode3_4scenes/mapper-steps-50000_view.pt",
-	'8' : "results/train_m3_full_dataset_bypassalpha5/mapper-steps-52500_view.pt",  
+	'8' : "results/mapper-steps-50000_view.pt",
 }
